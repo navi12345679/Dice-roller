2b4cf4381936903aab4268e14abc95a30a235319
navi12345679 <navithareddy879@gmail.com>
2024-06-04 13:19:24 +0530

    commit

---
+++ Index.js
@@ -0,0 +1,19 @@
+//Dice roller program
+
+function rolldice(){
+    const numofdice= document.getElementById("numofdice").value;
+    const diceresult= document.getElementById("diceresult");
+    const diceimages= document.getElementById("diceimages");
+
+   const values = [];
+   const images = [];
+
+   for(let i=0; i < numofdice; i++){
+    const value= Math.floor(Math.random() * 6) + 1;
+  //  console.log(value);
+    values.push(value);
+    images.push(`<img src="images/${value}.jpg" alt="Dice ${value}">`);
+   }
+      diceresult.textContent= `dice: ${values.join(' , ')}`;
+      diceimages.innerHTML= images.join('');
+}
